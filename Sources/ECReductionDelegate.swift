12fc074a5438b56fba802add2a2191cf6c8eb8ff
EvanCooper9 <evan.cooper@rogers.com>
2020-10-12 00:28:50 -0400

    Add some documentation

--- Sources/ECReductionDelegate.swift
+++ Sources/ECReductionDelegate.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+/// Some `UICollectionViewDelegate` methods require a return value. When multiple delegates are involved, it's hard to decide
+/// which value to take. Implement this method to handle these scenarios.
 public protocol ECReductionDelegate: AnyObject {
+    
+    /// - Parameters:
+    ///   - first: the first value obtained by a delegate (or default value)
+    ///   - second: the second value obtained by another delegate
+    ///   - selector: the delegate method being called
     func reduce<ReductionType>(_ first: ReductionType, _ second: ReductionType, selector: Selector) -> ReductionType
 }
